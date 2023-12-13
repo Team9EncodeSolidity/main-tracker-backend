bed4fb5f004295d3602118ae8cc7fc1e40446e88
andysign <andysignerdesigner@gmail.com>
2023-12-13 16:38:39 +0000

    Changed app ctrl to make it similar to latest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  SetTrackerCtAddrDto,
+  DeployTrackerContract,
+  GrantMintRole,
+  TaskIdToComplete,
+} from './dtos/app.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('/')
   getHello(): string {
@@ -20,29 +26,89 @@ export class AppController {
     return { result: await this.appService.getTokenAddress() };
   }
 
+  @Get('/token-contract-abi')
+  async getTokenAbi() {
+    return { result: await this.appService.getTokenAbi() };
+  }
+
   @Get('/tracker-contract-address')
   async getTrackerAddress() {
     return { result: await this.appService.getTrackerAddress() };
   }
 
+  @Get('/tracker-contract-abi')
+  async getTrackerAbi() {
+    return { result: await this.appService.getTrackerAbi() };
+  }
+
+  @Post('/deploy-token-contract')
+  async deployTokenContract(@Body() body = {}) {
+    return { result: await this.appService.deployTokenContract(body) };
+  }
+
+  @Post('/deploy-tracker-contract')
+  async deployTrackerContract(@Body() body: DeployTrackerContract) {
+    return { result: await this.appService.deployTrackerContract(body) };
+  }
+
   @Post('/set-tracker-contract-address')
-  async setTrackerCtAddr(@Body() body: { address: string }) {
+  async setTrackerCtAddr(@Body() body: SetTrackerCtAddrDto) {
     return { result: await this.appService.setTrackerCtAddr(body) };
+  }
+
+  @Post('/grant-mint-role')
+  async grantMintRole(@Body() body: GrantMintRole) {
+    return { result: await this.appService.grantMintRole(body) };
   }
 
   @Get('/tasks-list')
   async getTasksList() {
     return { result: await this.appService.getTasksList() };
   }
+
+  @Get('/task/:id')
+  async getTaskById(@Param('id') id: string) {
+    return { result: await this.appService.getTaskById(id) };
+  }
+
+  @Get('/nfts-list')
+  async getNftsList() {
+    return { result: await this.appService.getNftsList() };
+  }
+
+  @Get('/nft/:id')
+  async getNftById(@Param('id') id: string) {
+    return { result: await this.appService.getNftById(id) };
+  }
+
+  @Get('/nfts-decoded-list')
+  async getNftsDecodedList() {
+    return { result: await this.appService.getNftsDecodedList() };
+  }
+
+  @Get('/nft-decoded/:id')
+  async getNftDecodedById(@Param('id') id: string) {
+    return { result: await this.appService.getNftDecodedById(id) };
+  }
+
+  @Get('/balance-of/eth/:address')
+  async balanceOfEth(@Param('address') address: string) {
+    return { result: await this.appService.balanceOfEth(address) };
+  }
+
+  @Get('/balance-of/tokens/:address')
+  async balanceOfTokens(@Param('address') address: string) {
+    return { result: await this.appService.balanceOfTokens(address) };
+  }
+
+  @Post('/withdraw-treasury-eth')
+  async withdrawTreasuryEth(@Body() body = {}) {
+    return { result: await this.appService.withdrawTreasuryEth(body) };
+  }
+
+  @Post('/complete-task')
+  async completeTask(@Body() body: TaskIdToComplete) {
+    return { result: await this.appService.completeTask(body) };
+  }
+
 }
-
-// import { ApiProperty } from '@nestjs/swagger';
-
-// class SetTrackerCtAddrDto {
-//   @ApiProperty({
-//     type: String,
-//     required: true,
-//     default: '0x0000000000000000000000000000000000000000',
-//   })
-//   address: string;
-// }
